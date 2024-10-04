c7b48badaad7f9b6f7f9bcc51e8aee8da9a9dd42
Arturo Mora-Rioja <arturomora@gmail.com>
2024-10-04 17:00:50 +0200

    Styling improved

--- js/nav.js
+++ js/nav.js
@@ -9,7 +9,7 @@ document.querySelector('nav #about').addEventListener('click', () => {
         <p>
             Welcome to Food Repo, your ultimate destination for discovering diverse and delicious food recipes from around the globe. Whether you're craving a quick weeknight meal or an exotic culinary adventure, Food Repo offers a vast collection of recipes that cater to all tastes and skill levels. From hearty stews and refreshing salads to decadent desserts, our recipes feature a wide variety of ingredients—fresh, seasonal, and pantry staples. With contributions from different parts of the world, you'll experience the authentic flavors of Asian, Mediterranean, Latin American, and other international cuisines, all in one place. Each recipe comes with step-by-step instructions, nutritional information, and helpful tips to ensure you create a meal that's not only tasty but also wholesome. Join Food Repo today and explore the joy of cooking, one recipe at a time!
         </p>
-        <section>
+        <section id="address">
             <header>
                 <h3>Food Repo Inc.</h3>
             </header>
