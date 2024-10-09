e83d4a9cb9e8cde873c4610cceea3d1bc9e179b8
Arturo Mora-Rioja <arturomora@gmail.com>
2024-10-09 15:19:29 +0200

    Log in page implemented

--- js/login.js
+++ js/login.js
@@ -1,34 +1,24 @@
 import { baseUserUrl } from './common.js';
 
-document.querySelector('#frmSignup').addEventListener('submit', (e) => {
+document.querySelector('#frmLogin').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const password = e.target.txtPassword.value.trim();
-    const repeatPassword = e.target.txtRepeatPassword.value.trim();
-
-    if (password !== repeatPassword) {
-        alert('Passwords must match');
-        return false;
-    }
-
-    const firstName = e.target.txtFirstname.value.trim();
-    const lastName = e.target.txtLastname.value.trim();
     const email = e.target.txtEmail.value.trim();
+    const password = e.target.txtPassword.value.trim();
 
     const params = new URLSearchParams();
     params.append('email', email);
-    params.append('first_name', firstName);
-    params.append('last_name', lastName);
     params.append('password', password);
 
-    fetch(`${baseUserUrl}/users`, {
+    fetch(`${baseUserUrl}/validation`, {
         method: 'POST',
         body: params
     })
     .then(response => response.json())
     .then(data => {
         if (Object.keys(data).includes('user_id')) {
-            window.location.href = 'login.htm';
+            sessionStorage.setItem('food_repo_user_id', data.user_id);
+            window.location.href = 'index.html';
         } else {
             throw new Error(data.error);
         }
@@ -39,5 +29,5 @@ document.querySelector('#frmSignup').addEventListener('submit', (e) => {
             <p>Dear user, we are truly sorry to inform that there was an error while processing the data</p>
             <p class="error">${error}</p>
         `;        
-    });
+    })
 });
